d812b2821cc64c6a9a183cf6afb21aef9a112463
dag <45577604+dag25@users.noreply.github.com>
2024-08-14 21:46:06 +0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -39,17 +39,22 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
-    let decoded = '';
-    let currentWord = '';
-    for (let i = 0; i < expr.length; i++) {
-        if (MORSE_TABLE[expr.slice(i, i + 3)]) {
-            currentWord += MORSE_TABLE[expr.slice(i, i + 3)];
-        } else {
-            decoded += currentWord +'';
-            currentWord = '';
+    const space = '**********';
+    const codeSplit = expr.split(space);
+
+    const codeSplitDecode = codeSplit.map(item => {
+        let arrDecode = [];
+        for (let i = 0; i < item.length; i += 10) {
+            const decode = item
+                .slice(i, i + 10)
+                .replace(/10/g, '.')
+                .replace(/11/g, '-')
+                .replace(/00/g, '');
+            arrDecode.push(MORSE_TABLE[decode]);
         }
-    }
-    return decoded.trim();  // remove trailing space if any
+        return arrDecode.join('');
+    });
+    return codeSplitDecode.join(' ');
 }
 
 module.exports = {
